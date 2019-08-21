b611f7f9820a98a59acf4caedc25cbb5b6078b22
Jeiman Cabarcas <jcabarcas@cidenet.com.co>
2019-08-21 10:15:58 -0500

    Add repositories

---
+++ src/main/java/com/api/repository/KardexDetailRepository.java
@@ -0,0 +1,12 @@
+package com.api.repository;
+
+import com.api.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+
+@Repository("kardex_detail_repository")
+public interface KardexDetailRepository extends JpaRepository<ProductEntity, Serializable>{
+
+}
---
+++ src/main/java/com/api/repository/KardexHeaderRepository.java
@@ -0,0 +1,12 @@
+package com.api.repository;
+
+import com.api.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+
+@Repository("kardex_header_repository")
+public interface KardexHeaderRepository extends JpaRepository<ProductEntity, Serializable>{
+
+}
