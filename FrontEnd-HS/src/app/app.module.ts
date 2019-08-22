f14d552fb12c6b0ab921916ab7293bab08215dc2
Jeiman Cabarcas <jcabarcas@cidenet.com.co>
2019-08-22 09:04:43 -0500

    Frontend

---
+++ FrontEnd-HS/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { KardexComponent } from './components/kardex/kardex.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    KardexComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
